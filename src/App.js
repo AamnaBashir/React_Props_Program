23a71252445aa4d5a063de3d292427da97c6e51d
Aamna Bashir <aamnabashir47@gmail.com>
2020-12-07 23:09:46 +0500

    counter function added to use useState

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import City from './city';
 import Weather from './weather';
+import Counter from './counter';
 function App() {
   return (
     <div className="App">
        <City currentCity="Multan" nextCity="Lahore" />
        <hr />
        <Weather tempNow="17 degrees" tempNew="16 degrees" />
+       <hr />
+       <Counter />
     </div>
   );
 }
---
+++ src/counter.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+function Counter(){
+    let [count,setCount] =useState(1);
+   return(
+     <div>
+         <h1>Number of Items in cart:{count}</h1>
+         <button onClick="{
+             () => setCount(count++)
+         }">Add Items</button>
+     </div>
+   );
+}
+export default Counter;
