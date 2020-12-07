1cd2ede0bd1a9fffbe2d3c9dee56eaadc404f296
Aamna Bashir <aamnabashir47@gmail.com>
2020-12-07 20:51:59 +0500

    new component named weather added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import City from './city';
+import Weather from './weather';
 function App() {
   return (
     <div className="App">
        <City currentCity="Multan" nextCity="Lahore" />
+       <hr />
+       <Weather tempNow="17 degrees" tempNew="16 degrees" />
     </div>
   );
 }
